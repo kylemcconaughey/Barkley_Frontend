328ae191a58679afeca56905e7af41e1604b20c2
tleach01 <taylorleach0@gmail.com>
2020-11-06 14:18:38 -0500

    basic components/login/logout setup

---
+++ barkley/src/Components/Register.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { register } from './api.js'
+import { Redirect } from 'react-router-dom'
+
+function Register (props) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState(null)
+  const { token, onRegister } = props
+
+  if (token) {
+    return <Redirect to='/home' />
+  }
+
+  function registration (event) {
+    event.preventDefault()
+
+    register(username, password)
+      .then(function (token) {
+        setMessage('Welcome to Barkley!')
+        onRegister(token)
+      })
+  }
+
+  return (
+    <div>
+      {message &&
+        <div>
+          {message}
+        </div>}
+      <div className='signup'>Sign Up for Barkley</div>
+      <form onSubmit={registration} className='register'>
+        <label
+          htmlFor='username'
+        />
+        <input
+          type='text' className='form' placeholder='Username' name='username' value={username}
+          onChange={event => setUsername(event.target.value)}
+        />
+        <label
+          htmlFor='password'
+        />
+        <input
+          type='password' className='form' placeholder='Password' name='password' value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        <label
+          htmlFor='email'
+        />
+        <button className='formBtn' type='submit'>
+            Sign Up
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Register
--- barkley/src/Components/api.js
+++ barkley/src/Components/api.js
@@ -3,3 +3,18 @@ import axios from 'axios'
 const request = axios.create({
   baseURL: 'https://brkly.herokuapp.com/'
 })
+
+export function getToken (username, password) {
+  return request.post('/auth/token/login/', {
+    username: username,
+    password: password
+  }).then(response => response.data.auth_token)
+}
+
+export function register (username, password) {
+  return axios.post('https://brkly.herokuapp.com//auth/users/', {
+    username: username,
+    password: password
+  })
+    .then(res => res.token)
+}
