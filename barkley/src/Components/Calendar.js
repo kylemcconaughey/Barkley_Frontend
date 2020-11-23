caba69e181a96bc6ee82c7eb09b044e41e4623b4
tleach01 <taylorleach0@gmail.com>
2020-11-22 21:02:16 -0500

    calendar kind of where we want it? Need to fix event title when adding new event

--- barkley/src/Components/Calendar.js
+++ barkley/src/Components/Calendar.js
@@ -1,97 +1,138 @@
-import React, { Component } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
+import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 // import events from "./ReactBigCalendar/events";
-import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
-
-import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
 // lodash??
+const DnDCalendar = withDragAndDrop(Calendar)
 
-const localizer = momentLocalizer(moment);
-const DnDCalendar = withDragAndDrop(Calendar);
+const localizer = momentLocalizer(moment)
 
-class CalendarPost extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      events:[
-        {
-          start: moment().toDate(),
-          end: moment().add(0, "days").toDate(),
-          title: "Today",
-        },
-        {
-          id: 4,
-          title: 'Dog Park Playdate',
-          start: new Date(2020, 10, 28, 2, 0, 0),
-          end: new Date(2020, 10, 28, 5, 0, 0),
-          desc: '910 Brookside Dr, Raleigh, NC 27604'
-        },
-        {
-          title: 'Lunch at Babymoon Cafe',
-          start: new Date(2020, 11, 11, 11, 0, 0, 0),
-          end: new Date(2020, 11, 11, 11, 0, 0, 0),
-          desc: 'Address: 100 Jerusalem Dr #106'
-        },
-        {
-          id: 8,
-          title: 'Birthday Party for Moose',
-          start: new Date(2020, 10, 29, 9, 0, 0),
-          end: new Date(2020, 10, 29, 11, 30, 0)
-        }
-      ],
-    };
-
-    this.moveEvent = this.moveEvent.bind(this);
+const initialEvents = [
+  {
+    id: 0,
+    title: 'Dog Park ',
+    allDay: true,
+    start: new Date(2020, 10, 12, 10, 30, 0, 0),
+    end: new Date(2020, 10, 12, 12, 30, 0, 0),
+    desc: 'Address: 910 Brookside Dr, Raleigh, NC 27604'
+  },
+  {
+    id: 3,
+    title: 'Some Event',
+    start: new Date(2020, 11, 9, 0, 0, 0),
+    end: new Date(2020, 11, 9, 0, 0, 0)
+  },
+  {
+    id: 4,
+    title: 'Dog Park Playdate',
+    start: new Date(2020, 11, 11),
+    end: new Date(2020, 11, 11),
+    desc: '910 Brookside Dr, Raleigh, NC 27604'
+  },
+  {
+    id: 5,
+    title: 'Zoom meeting',
+    start: new Date(2020, 11, 12, 10, 30, 0, 0),
+    end: new Date(2020, 11, 12, 12, 30, 0, 0),
+    desc: 'Pre-meeting meeting, to check for catfish'
+  },
+  {
+    id: 6,
+    title: 'Lunch at Babymoon Cafe',
+    start: new Date(2020, 11, 12, 12, 0, 0, 0),
+    end: new Date(2020, 11, 12, 13, 0, 0, 0),
+    desc: 'Address: 100 Jerusalem Dr #106'
+  },
+  {
+    id: 7,
+    title: 'Meeting',
+    start: new Date(2020, 11, 12, 14, 0, 0, 0),
+    end: new Date(2020, 11, 12, 15, 0, 0, 0)
+  },
+  {
+    id: 8,
+    title: 'Birthday Party for Moose',
+    start: new Date(2020, 11, 13, 7, 0, 0),
+    end: new Date(2020, 11, 13, 10, 30, 0),
+    desc: 'Address: 412 Scott Lane, Wallingford, PA 19086'
+  },
+  {
+    id: 9,
+    title: 'Today',
+    start: new Date(new Date().setHours(new Date().getHours() - 3)),
+    end: new Date(new Date().setHours(new Date().getHours() + 3))
   }
+]
 
-  moveEvent({ event, start, end }) {
-    const { events } = this.state;
-
-    const idx = events.indexOf(event);
-    const updatedEvent = { ...event, start, end };
-
-    const nextEvents = [...events];
-    nextEvents.splice(idx, 1, updatedEvent);
-
-    this.setState({
-      events: nextEvents
-    });
-  }
-
-  resizeEvent = (resizeType, { event, start, end }) => {
-    const { events } = this.state;
-
-    const nextEvents = events.map(existingEvent => {
-      return existingEvent.id === event.id
-        ? { ...existingEvent, start, end }
-        : existingEvent;
-    });
-
-    this.setState({
-      events: nextEvents
-    });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <DnDCalendar
-        selectable
-          defaultDate={moment().toDate()}
-          defaultView="month"
-          events={this.state.events}
-          localizer={localizer}
-          onEventDrop={this.moveEvent}
-          onEventResize={this.resizeEvent}
-          resizable
-          style={{ height: "100vh" }}
-        />
-      </div>
-    );
-  }
+const onEventDrop = ({ event, start, end, allDay }) => {
+  console.log('event clicked')
+  console.log(start, event, end, allDay)
 }
 
+const Scheduler = () => {
+  const [events, setEvents] = React.useState(initialEvents)
 
-export default CalendarPost;
+  const addEvent = ({ event, start, end, allDay }) => {
+    const newEvent = {
+      id: events.length,
+      title: 'New event',
+      start: new Date(new Date(start).setHours(new Date().getHours() - 3)),
+      end: new Date(new Date(end).setHours(new Date().getHours() + 3)),
+      desc: 'This is a new event'
+    }
+
+    setEvents(state => [...state, newEvent])
+  }
+
+  return (
+    <>
+      <div className='wrapper' style={{ minHeight: '100vh' }}>
+        <DnDCalendar
+          selectable
+          events={events}
+          startAccessor='start'
+          endAccessor='end'
+          defaultDate={moment().toDate()}
+          defaultView='month'
+          localizer={localizer}
+          toolbar
+          resizable
+          style={{ height: '100vh' }}
+          onEventDrop={onEventDrop}
+          components={{
+            event: EventComponent,
+            agenda: {
+              event: EventAgenda
+            }
+          }}
+          onSelectSlot={addEvent}
+          onSelectEvent={event => alert(event.desc)}
+        />
+      </div>
+    </>
+  )
+}
+
+export default Scheduler
+
+const EventComponent = ({ start, end, title }) => {
+  return (
+    <>
+      <p>{title}</p>
+      <p>{start}</p>
+      <p>{end}</p>
+    </>
+  )
+}
+
+const EventAgenda = ({ event }) => {
+  return (
+    <span>
+      <em style={{ color: 'blue' }}>{event.title}</em>
+      <p>{event.desc}</p>
+    </span>
+  )
+}
