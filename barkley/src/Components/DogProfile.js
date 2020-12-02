ad5b545f27474ae9f6c0aa8dde2e53528dc2834a
tleach01 <taylorleach0@gmail.com>
2020-12-02 07:49:57 -0500

    changing the link to for users profiles, adding posts

--- barkley/src/Components/DogProfile.js
+++ barkley/src/Components/DogProfile.js
@@ -15,7 +15,7 @@ function DogProfile ({ dog }) {
         <div>Breed:{dog.breed}</div>
         <div>Good in group sizes: {dog.group_size}</div>
         <div>Vaccinated: {dog.vaccinated}</div>
-        <div>Owner: <Link to='/profile'><em>{dog.owner.username} </em></Link><em> </em></div>
+        <div>Owner: <Link to={`/profile/${dog.owner.id}/`}><em>{dog.owner.username} </em></Link><em> </em></div>
       </div>
     </div>
   )
